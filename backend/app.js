e6f7566f1e9ff68843259a4720516d5774ffb638
batuhanBASAK <batuhanbasak1997@gmail.com>
2025-04-29 20:13:05 +0300

    v1

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT
 
 const profileInfoAPI = require('./profileInfo')
 
+const searchPeopleAPI = require('./searchPeople')
+
+
+// returns the data of the user, linkedIn username has been given as parameter
 const getProfileInfo = async (username) => {
     try {
         const response = await profileInfoAPI.getResponse(username)
@@ -20,13 +24,34 @@ const getProfileInfo = async (username) => {
 }
 
 // Debugging profileInfoAPI
-getProfileInfo('batuhan-basak')
-  .then(profileData => {
-    console.log(profileData);
-  })
-  .catch(err => {
-    console.error('Error fetching profile info:', err);
-  });
+// getProfileInfo('batuhan-basak')
+//   .then(profileData => {
+//     console.log(profileData);
+//   })
+//   .catch(err => {
+//     console.error('Error fetching profile info:', err);
+//   });
+
+
+// keyword: job name
+// start: page number
+const getPeopleSearched = async (keywords, start) => {
+    try {
+        const response = await searchPeopleAPI.getResponse(keywords, start)
+        return response.data;
+    } catch (err) {
+        console.error(`Failed during searching people`, err);
+        throw err;
+    }
+}
+
+
+// Debugging getPeopleSearched
+getPeopleSearched('data science', 0)
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => console.log(err))
 
 
 
---
+++ backend/searchPeople.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+require('dotenv').config()
+
+
+const API_KEY = process.env.RAPIDAPI_KEY
+
+module.exports = {
+    getResponse : (keywords, start) => axios.request({
+        method: 'GET',
+        url: 'https://linkedin-data-api.p.rapidapi.com/search-people',
+        params: {
+            keywords: keywords,
+            start: start
+        },
+        headers: {
+            'x-rapidapi-key': API_KEY,
+            'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com'
+        }
+    })
+}
